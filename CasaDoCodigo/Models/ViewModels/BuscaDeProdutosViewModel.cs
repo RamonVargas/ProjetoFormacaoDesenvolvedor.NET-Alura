3b278b4c466b1d02cf42774b1f1529f37524f673
Ramon Vargas Maldonado <jehutyof@msn.com>
2018-09-22 13:13:37 -0300

    Conclusão do projeto de formação

---
+++ CasaDoCodigo/Models/ViewModels/BuscaDeProdutosViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+    public class BuscaDeProdutosViewModel
+    {
+        public BuscaDeProdutosViewModel(IList<Produto> produtos,string pesquisa)
+        {
+            Produtos = produtos;
+            Pesquisa = pesquisa;
+        }
+
+        public IList<Produto> Produtos { get; }
+        public string Pesquisa { get; }
+    }
+}
---
+++ CasaDoCodigo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,44 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+
+    public interface ICategoriaRepository
+    {
+        Task Salvar(string nome);
+        Categoria GetCategoriaPorNome(string nome);
+    }
+    public class CategoriaRepository : BaseRepository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+
+        public Categoria GetCategoriaPorNome(string nome)
+        {
+            return
+                dbSet
+                .Where(c => c.Nome == nome)
+                .SingleOrDefault();
+        }
+
+        public async Task Salvar(string nome)
+        {
+            var categoria = dbSet
+                                        .Where(c => c.Nome == nome)
+                                        .SingleOrDefault();
+            if (categoria == null)
+            {
+                categoria = new Categoria(nome);
+                contexto.Add(categoria);
+            }
+            await contexto.SaveChangesAsync();
+        }
+    }
+}
+
+
